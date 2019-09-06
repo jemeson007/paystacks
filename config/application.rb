d5496f2edfa20caefdf71f324e7f09d875c5eda3
jemeson007 <emesonjonah@gmail.com>
2019-09-06 15:27:54 +0100

    Aut

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,25 @@ module Fn
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    ActionMailer::Base.smtp_settings = {
+    	# :address => 'smtp.gmail.com',
+    	# :domain => 'mail.google.com',
+    	# :port => 587,
+    	# :user_name  => 'emesonjonah@gmail.com',
+    	# :password => "$parrow$12@4",
+    	# :authentication => 'login',
+    	# :enable_starttls_auto => true
+
+
+
+                           :address   => 'smtp.sendgrid.net',
+                           :port      => 587,
+                           :domain    => 'sendgrid.com',
+                           :user_name => 'dataflux',
+                           :password  => '#parrow#12',
+                           :authentication => 'plain',
+                           :enable_starttls_auto => true
+    }
   end
 end
